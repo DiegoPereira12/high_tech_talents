13785fa0fa5f11f80f845f7008221f88acf06561
Diego Junior Pereira <diego.pereira@softplan.com.br>
2022-03-16 20:03:04 -0300

    importando exercicios realizados de 5 => 7

---
+++ exercicios_python/exercicio_05.py
@@ -0,0 +1,11 @@
+# Faça um Programa que converta metros para centímetros
+
+print('Convertendo metros em centímentros')
+
+print(30 * '=')
+
+m = float(input('Digite o valor a ser convertido => '))
+
+cm = m * 100
+
+print(f'A medida de {m} metro(s), corresponde a {cm} centímentro(s).')
---
+++ exercicios_python/exercicio_06.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = float(input('Digite o raio do circulo => '))
+
+area = 3.14 * (raio ** 2)
+
+print(f'Á area do circulo é igual {area}')
