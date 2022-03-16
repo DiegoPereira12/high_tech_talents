4739bb49f4836f527dff420f410a0f5380627707
Diego Junior Pereira <diego.pereira@softplan.com.br>
2022-03-16 20:31:22 -0300

    importando exercicio realizado 7

---
+++ exercicios_python/exercicio_07.py
@@ -0,0 +1,9 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário
+
+altura = float(input('Digite a altura do quadrado => '))
+largura = float(input('Digite a largura do quadrado => '))
+
+area = altura * largura
+dobro = (area * 2)
+
+print(f'Se a altura e {altura} e a largura{largura} o dobro dessa área é {dobro}')
