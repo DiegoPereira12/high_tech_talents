5d43526fd952c092f3132b5982a79eb1f6bdfb41
Diego Junior Pereira <diego.pereira@softplan.com.br>
2022-03-17 13:50:43 -0300

    importando exercicio realizado 9

---
+++ exercicios_python/exercicio_09.py
@@ -0,0 +1,12 @@
+# Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+print('Conversor de temperatura: De Fahrenheit Para Celsius')
+
+print('=' * 55)
+
+f = float(input('Digite temperatura em Graus Fahrenheit => '))
+
+c = 5 * ((f-32) / 9)
+
+print(f'{f} graus em Fahrenheit, é equivalante a {c:.2f} graus Celsius')
+
