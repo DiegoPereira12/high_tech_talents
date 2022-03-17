f10a6705a0d215221dd03350cdb3416e0183c3df
Diego Junior Pereira <diego.pereira@softplan.com.br>
2022-03-17 13:52:22 -0300

    importando exercicio realizado 8

---
+++ exercicios_python/exercicio_08.py
@@ -0,0 +1,13 @@
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês
+# Calcule e mostre o total do seu salário no referido mês.
+
+print('$$$$ Cálculo Salário $$$')
+
+print('=' *30)
+
+valor = float(input('Quanto você ganha por hora? R$ : '))
+hora = float(input('Quantas horas você trabalhou?: '))
+
+salario = valor * hora
+
+print(f'O valor recebido por hora é {valor} reais, vezes {hora} horas trabalhadas, seu salário é esse mês é R${salario} reais')
