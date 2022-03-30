21ed0da0720e4a8fb5802fbc0b5fe204212edcc5
Diego Junior Pereira <diego.pereira@softplan.com.br>
2022-03-30 20:19:02 -0300

    [200~

--- exercicios_python/exercicio_15.py
+++ exercicios_python/exercicio_15.py
@@ -9,11 +9,41 @@
 # Quanto pagou ao sindicato
 # Salário líquido
 
-nome = input('Digite seu nome => ')
+from time import sleep
+
+print('=' * 33)
+print('Olá, vamos calcular o seu salário')
+print('=' * 33)
+
+nome_funcionario = input('Digite seu nome => ')
 valor = float(input('Quanto você ganha por hora? R$ : '))
-hora = float(input('Quantas horas você trabalhou?: '))
+horas_trabalhadas = float(input('Quantas horas você trabalhou?: '))
 
-salario = valor * hora
+salario_bruto = valor * horas_trabalhadas
 
-print(f'O valor recebido por hora é {valor} reais, vezes {hora} horas trabalhadas, seu salário BRUTOé esse mês é R${salario} reais.')
-print()
+print('=' * 111)
+print(f'O valor recebido por hora é {valor} reais, vezes {horas_trabalhadas} horas trabalhadas, seu salário BRUTO esse mês é R$ {salario_bruto} reais.')
+print('=' * 111)
+
+print('Aguarde enquanto calculamos os descontos ...\n')
+sleep(5)
+
+ir = salario_bruto * 0.11
+inss = salario_bruto * 0.08
+sindicato = salario_bruto * 0.05
+
+print(f'Seu salario BRUTO é de {salario_bruto} reais, menos 11% de desconto do Imposto de Renda é {salario_bruto - ir} reais')
+sleep(5)
+print(f'Seu salario BRUTO é de {salario_bruto} reais, menos 8% de desconto do INSS é {salario_bruto - inss} reais')
+sleep(5)
+print(f'Seu salario BRUTO é de {salario_bruto} reais, menos 5% de desconto do Sindicato é {salario_bruto - sindicato} reais\n')
+
+total_descontos = ir + inss + sindicato
+
+print(f'Total de descontos é igual {total_descontos} reais\n')
+
+salario_liquido = salario_bruto - ir - inss - sindicato
+
+print('=' * 48)
+print(f'{nome_funcionario} seu Salário Líquido nesse mês é {salario_liquido} reais.')
+print('=' * 48)
