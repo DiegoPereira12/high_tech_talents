f6a2e1c1d7bcc0fc82ded6b6149be92a325bd124
unknown <diegoo.junior55@gmail.com>
2022-04-02 14:25:10 -0300

    importando atividade poo meio de transporte(em construção)

---
+++ Atividades práticas em aula/poo_heranças/meio_de_transporte/meio_de_transporte.py
@@ -0,0 +1,26 @@
+class MeioDeTransporte():
+    pass
+
+class TransporteTerreste():
+
+    def carro():
+        pass
+
+    def caminhao():
+        pass
+
+class TransporteAquatico():
+    
+    def barco():
+        pass
+
+    def lancha():
+        pass
+
+class TransporteAreo():
+
+    def aviao():
+        pass
+
+    def helicoptero():
+        pass
