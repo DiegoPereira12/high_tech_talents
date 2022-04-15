a632dcf1b319aebafba354b2413c91fa038a2807
unknown <diegoo.junior55@gmail.com>
2022-04-15 14:07:57 -0300

    importando atividade flask_sqlalchemy

--- criando aplicações/flask_sqlachemy/Scripts/activate.bat
+++ criando aplicações/flask_sqlachemy/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Documents\high_tech_talents_desktop\flask_sqlachemy
+set VIRTUAL_ENV=C:\Users\User\Documents\high_tech_talents\criando aplicações\flask_sqlachemy
 
 if not defined PROMPT set PROMPT=$P$G
 
